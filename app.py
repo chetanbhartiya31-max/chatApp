0fcbddc0196670c1c0a881d68d4b50d6c729b6ad
chetanbhartiya31-max <chetanbhartiya31@gmail.com>
2026-05-30 09:40:57 -0700

    Refactor app.py for better readability and structure

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
     email_valadity = db.Column(db.Boolean, default=True)
@@ -68,7 +68,7 @@ class Messages(db.Model):
 class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
     messages = db.relationship("Messages", backref="room")
 
